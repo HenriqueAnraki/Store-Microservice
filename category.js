0c3707b957554e001554f3d0fd65d29ba912cf5b
Henrique <henrique_anraki@outllok.com>
2018-05-25 18:15:27 -0300

    simple read

--- category.js
+++ category.js
@@ -2,6 +2,17 @@ const { json, createError } = require('micro')
 
 const Category = require('./models/Category')
 
+const getCategoryById = async (req) => {
+  const {id} = req.query
+
+  if (!id) throw createError(404, 'Category not found.')
+
+  const category = await Category.query().findById(id)
+  // .eager('products') -> error: relationMappings do products (duas relaçoes)
+
+  return category
+}
+
 const getCategoryByName = async (name) => {
   const category = await Category.query().where('name', name)
 
@@ -9,19 +20,25 @@ const getCategoryByName = async (name) => {
 }
 
 const createCategory = async (req, res) => {
-  console.log('create')
   const data = await json(req)
   const { name } = data
 
   const category = await getCategoryByName(name)
 
   if (category.length > 0) throw createError(401, 'Category already exist.')
-  console.log('inserting category')
+
   await Category.query().insert({name: name})
 }
 
 const readCategory = async (req, res) => {
+  const category = await getCategoryById(req)
 
+  if (!category || category.length <= 0) throw createError(404, 'Category not found.')
+
+  console.log('Category found: ', category)
+
+  // return result (json) in response
+  return category
 }
 
 const updateCategory = async (req, res) => {
